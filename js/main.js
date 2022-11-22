7d4d7d4780ae97b1a9709d5268d3c7aa9348fcb7
mariiaws <wooglay@gmail.com>
2022-11-22 14:12:58 +0300

    Added the long sticks for background.

--- js/main.js
+++ js/main.js
@@ -24,8 +24,31 @@ hamburger.addEventListener('click', function () {
 })
 
 
+//animation long stick
 
+let container = document.querySelector(".container");
+let ball = document.querySelector(".stick-one");
 
+let maxX = container.clientWidth - ball.offsetWidth;
+let maxY = container.clientHeight - ball.offsetHeight;
 
+let size = 30;
+let start = null;
+let duration = 1;
 
+function step(timestamp) {
+    let progress, x, y;
+    if (start === null) start = timestamp;
 
+    progress = (timestamp - start) / duration / 2000;
+
+    x = progress * maxX / size;
+    y = 2 * Math.sin(x);
+
+    ball.style.left = Math.min(maxX, size * x) + "px";
+    ball.style.top = maxY / 2 + (size * y) + "px";
+
+    if (progress >= 1) start = null;
+    requestAnimationFrame(step);
+}
+requestAnimationFrame(step);
