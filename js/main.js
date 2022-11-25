7c102cc1eaa34ad9253b48e14af0a36b95840633
mariiaws <wooglay@gmail.com>
2022-11-25 21:30:24 +0300

    added parallax variables

--- js/main.js
+++ js/main.js
@@ -14,7 +14,6 @@ hamburger.addEventListener('click', function () {
         navWrapper = document.querySelector('.nav-wrapper').style.display = 'flex';
     }
 
-
     lineTop.classList.toggle('line-top__animate');
     lineMiddle.classList.toggle('line-middle__animate');
     lineBottom.classList.toggle('line-bottom__animate');
@@ -22,3 +21,34 @@ hamburger.addEventListener('click', function () {
     console.log('click')
 
 })
+
+//Parallax
+
+let container = document.querySelector('.container');
+
+let coffeeStand = document.querySelector('.coffee-stand');
+let coffeeStandStripes = document.querySelector('.coffee-stand-stripes');
+let cone = document.querySelector('.cone');
+let glass = document.querySelector('.glass');
+let littleCone = document.querySelector('.little-cone');
+let longStickLightPink = document.querySelector('.long-stick-light-pink-zero');
+let longStickLightPinkOne = document.querySelector('.long-stick-light-pink-one');
+let longStickDark = document.querySelector('.long-stick-dark');
+let middleBall = document.querySelector('.middle-ball');
+let semicircle = document.querySelector('.semicircle');
+let theBall = document.querySelector('.the-ball');
+let theBigBall = document.querySelector('.the-big-ball');
+let violetCone = document.querySelector('.violet-cone');
+
+
+
+
+
+container.addEventListener('pointermove', function () {
+console.log('pointermove');
+
+
+    
+
+})
+
